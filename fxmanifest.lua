ed4ab054be4f4af99df4122c09ee02b5165b03e0
tayonnnn <tayonnnn@gmail.com>
2025-05-21 11:39:38 -0400

    chore: simple folder structure started.

---
+++ fxmanifest.lua
@@ -0,0 +1,52 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'tayon'
+description 'Ignite Core - A modern, modular FiveM framework'
+version '0.0.1'
+
+lua54 'yes'
+
+-- Define shared files
+shared_scripts {
+    '@ox_lib/init.lua', -- dependency
+    --'shared/main.lua',
+    --'shared/utils.lua',
+    --'shared/enums.lua',
+    --'config/*.lua'
+}
+
+-- Server-side scripts
+server_scripts {
+    '@oxmysql/lib/MySQL.lua', -- Database library
+    --'sv/main.lua',
+    --'sv/auth.lua',
+    --'sv/database.lua',
+    --'sv/events.lua',
+    --'sv/players.lua'
+}
+
+-- Client-side scripts
+client_scripts {
+    --'cl/main.lua',
+    --'cl/spawn.lua',
+    --'cl/ui.lua',
+    --'cl/notifications.lua'
+}
+
+-- Optional NUI integration
+--ui_page 'webui/index.html'
+
+files {
+    --'webui/index.html',
+    --'webui/js/*.js',
+    --'webui/css/*.css',
+    --'webui/fonts/*',
+    --'webui/images/*'
+}
+
+-- Dependencies (optional)
+dependencies {
+    'oxmysql',
+    'ox_lib'
+}
